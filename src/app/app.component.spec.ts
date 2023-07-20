ed33f2e6c07294a41f436acc322b4b4bde3555c6
Hernan Ontivero <hernanontivero559@gmail.com>
2023-07-19 21:12:01 -0300

    App en Angular con angular material, ngIf.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
   }));
 
   it('should create the app', () => {
@@ -14,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular_Academy'`, () => {
+  it(`should have a title 'my-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular_Academy');
+    expect(app.title).toEqual('my-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular_Academy app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-app!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  nombre: string;
+  costo: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_Academy';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  items: Item[] = [
+    { nombre: 'Producto 1', costo: '$10' },
+    { nombre: 'Producto 2', costo: '$20' },
+    { nombre: 'Producto 3', costo: '$30' }
+  ];
+  mostrarLista = true;
+
+  toggleList() {
+    this.mostrarLista = !this.mostrarLista;
+  }
 }
